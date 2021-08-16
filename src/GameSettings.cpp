32c356efa74fff6a6b045964fe4eea571fb35e1d
zgmnt <zygmunt.latyszewicz@outlook.com>
2021-08-16 14:32:32 +0200

    read from configGame in GameSettings

--- src/GameSettings.cpp
+++ src/GameSettings.cpp
@@ -1,6 +1,6 @@
 #include "GameSettings.h"
 #include <Windows.h>
-
+#include <fstream>
 void GameSettings::load(int& WIDTH, int& HEIGHT)
 {
 	font.loadFromFile("fonts\\mrsmonster.ttf");
@@ -24,23 +24,32 @@ void GameSettings::load(int& WIDTH, int& HEIGHT)
 	text_back.setPosition(WIDTH / 10, 510);
 
 
-	// text  board size
-	text_board_size.setFont(font);
-	text_board_size.setString("BOARD SIZE");
-	text_board_size.setOutlineColor(sf::Color::Red);
-	text_board_size.setFillColor(sf::Color::Yellow);
-	text_board_size.setOutlineThickness(2);
-	text_board_size.setCharacterSize(35);
-	text_board_size.setPosition(WIDTH / 10, 250);
+	// text  board size X
+	text_board_sizeX.setFont(font);
+	text_board_sizeX.setString("BOARD SIZE X ");
+	text_board_sizeX.setOutlineColor(sf::Color::Red);
+	text_board_sizeX.setFillColor(sf::Color::Yellow);
+	text_board_sizeX.setOutlineThickness(2);
+	text_board_sizeX.setCharacterSize(35);
+	text_board_sizeX.setPosition(WIDTH / 10, 200);
+	// text  board size Y
+	text_board_sizeY.setFont(font);
+	text_board_sizeY.setString("BOARD SIZE Y ");
+	text_board_sizeY.setOutlineColor(sf::Color::Red);
+	text_board_sizeY.setFillColor(sf::Color::Yellow);
+	text_board_sizeY.setOutlineThickness(2);
+	text_board_sizeY.setCharacterSize(35);
+	text_board_sizeY.setPosition(WIDTH / 10, 250);
+	
 
 	// text  snake speed
 	text_snake_speed.setFont(font); 
-	text_snake_speed.setString("SNAKE SPEED");
+	text_snake_speed.setString("SNAKE SPEED ");
 	text_snake_speed.setOutlineColor(sf::Color::Red);
 	text_snake_speed.setFillColor(sf::Color::Yellow);
 	text_snake_speed.setOutlineThickness(2);
 	text_snake_speed.setCharacterSize(35);
-	text_snake_speed.setPosition(WIDTH / 10, 200);
+	text_snake_speed.setPosition(WIDTH / 10, 300);
 
 	// text screen resolution
 	text_screen_resolution.setFont(font);
@@ -51,16 +60,48 @@ void GameSettings::load(int& WIDTH, int& HEIGHT)
 	text_screen_resolution.setCharacterSize(50);
 	text_screen_resolution.setPosition(WIDTH / 10, 10);
 
+	// text constrol
+	text_control_WSAD.setFont(font);
+	text_control_WSAD.setString("CONTROL TYPE WSAD ");
+	text_control_WSAD.setOutlineColor(sf::Color::Red);
+	text_control_WSAD.setFillColor(sf::Color::Yellow);
+	text_control_WSAD.setOutlineThickness(2);
+	text_control_WSAD.setCharacterSize(35);
+	text_control_WSAD.setPosition(WIDTH / 10, 450);
 
+	// text music
+	text_music.setFont(font);
+	text_music.setString("MUSIC ");
+	text_music.setOutlineColor(sf::Color::Red);
+	text_music.setFillColor(sf::Color::Yellow);
+	text_music.setOutlineThickness(2);
+	text_music.setCharacterSize(35);
+	text_music.setPosition(WIDTH / 10, 400);
+
+	// text obstacles
+	text_obstacles.setFont(font);
+	text_obstacles.setString("OBSTACLES " );
+	text_obstacles.setOutlineColor(sf::Color::Red);
+	text_obstacles.setFillColor(sf::Color::Yellow);
+	text_obstacles.setOutlineThickness(2);
+	text_obstacles.setCharacterSize(35);
+	text_obstacles.setPosition(WIDTH / 10, 350);
+
+	getConfigData();
 }
 
 void GameSettings::draw(sf::RenderWindow& W)
 {
 	W.draw(text_back);
 	W.draw(text_default);
-	W.draw(text_board_size);
+	W.draw(text_board_sizeX);
+	W.draw(text_board_sizeY);
 	W.draw(text_snake_speed);
 	W.draw(text_screen_resolution);
+	W.draw(text_obstacles);
+	W.draw(text_music);
+	W.draw(text_control_WSAD);
+
 }
 Switcher GameSettings::update(sf::RenderWindow& W)
 {
@@ -96,4 +137,30 @@ ScreenResolution GameSettings::getScreenResolution()
 	screen_resolution.height = handle_screen.bottom;
 
 	return screen_resolution;
+}
+
+void GameSettings::getConfigData()
+{
+	std::vector<sf::Text*> texts 
+	{
+		& text_board_sizeX ,
+		& text_board_sizeY, 
+		&text_snake_speed,
+		&text_obstacles,
+		&text_music,
+		& text_control_WSAD
+
+	};
+	std::fstream file;
+	file.open("game_config.txt");
+	std::string temp;
+
+	for (auto it : texts)
+	{
+		file >> temp;
+		temp = temp.substr(temp.size() - 1, temp.size());
+		if (temp == "N") { temp = "ON"; }
+		else if (temp == "F") { temp = "OFF"; }
+		it->setString(it->getString() + temp);
+	}
 }
--- src/GameSettings.h
+++ src/GameSettings.h
@@ -9,22 +9,25 @@ struct ScreenResolution
 
 class GameSettings
 {
-	bool isFullScreen = false;
-
 	sf::Font font;
 
 	// texts
 	sf::Text text_back;
 	sf::Text text_default;
 	sf::Text text_snake_speed;
-	sf::Text text_board_size;
+	sf::Text text_board_sizeX;
+	sf::Text text_board_sizeY;
 	sf::Text text_screen_resolution;
+	sf::Text text_obstacles;
+	sf::Text text_music;
+	sf::Text text_control_WSAD;
 
 	// fullscreen icon
 	sf::Texture fullscreen_icon_texture;
 	sf::Sprite fullscreen_icon_sprite;
 	// private functions //
 	ScreenResolution getScreenResolution();
+	void getConfigData();
 
 public:
 	void load(int& WIDTH, int& HEIGHT);
